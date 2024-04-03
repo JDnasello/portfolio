7c09b99aa0924db019f4f7477d5a635724a89ea1
JDnasello <jnasellopron@gmail.com>
2024-04-03 19:53:33 -0300

    cambiando metodo de importacion del loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,18 @@
-import 'ldrs/trefoil'
+import { trefoil } from "ldrs"
+
+trefoil.register('l-trefo')
 
 const Loader = () => {
     return (
         <div className='container-loader'>
-            <l-trefoil
+            <l-trefo
             size="150"
             stroke="9"
             stroke-length="0.15"
             bg-opacity="0.1"
             speed="1.4"
             color="var(--sky-blue)" 
-            ></l-trefoil>
+            ></l-trefo>
         </div>
     )
 }
